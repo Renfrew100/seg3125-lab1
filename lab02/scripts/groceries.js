81dcbe195fb90ae43441498d3d0c2f06d74e697b
Ali Aftab, Muhammad <aliaftab9@hotmail.com>
2021-05-30 21:41:27 -0400

    changed directory content and added lab2

---
+++ lab02/scripts/groceries.js
@@ -0,0 +1,72 @@
+var products = [
+	{
+		name: "Chicken",
+		lactoseFree: true,
+		nutsFree: true,
+        organic: true,
+		price: 1.00
+	},
+    {
+		name: "Bread",
+		lactoseFree: false,
+		nutsFree: true,
+		organic: true,
+        price: 1.25
+	},
+    {
+		name: "Milk",
+		lactoseFree: false,
+		nutsFree: true,
+		organic: true,
+        price: 1.70
+	},
+    {
+		name: "Butter",
+		lactoseFree: false,
+		nutsFree: true,
+		organic: true,
+        price: 1.80
+	},
+    {
+		name: "Cheese",
+		lactoseFree: true,
+		nutsFree: true,
+		organic: false,
+        price: 1.83
+	},
+    {
+		name: "Oranges",
+		lactoseFree: true,
+		nutsFree: true,
+		organic: false,
+        price: 2.05
+	},
+    {
+		name: "Yogurt",
+		lactoseFree: false,
+		nutsFree: true,
+		organic: true,
+        price: 2.10
+	},
+    {
+		name: "Peanuts",
+		lactoseFree: true,
+		nutsFree: false,
+		organic: false,
+        price: 2.50
+	},
+    {
+		name: "Mint",
+		lactoseFree: true,
+		nutsFree: true,
+		organic: false,
+        price: 2.53
+	},
+    {
+		name: "Pasta",
+		lactoseFree: true,
+		nutsFree: true,
+		organic: true,
+        price: 3.05
+	},
+];
