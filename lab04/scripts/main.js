4bf7b6078f3272694805f31d1f4689cd6668ca1e
Ali Aftab, Muhammad <aliaftab9@hotmail.com>
2021-06-14 08:08:34 -0400

    added services and experts

--- lab04/scripts/main.js
+++
@@ -1,7 +0,0 @@
-function selectTab(tab){
-    document.getElementById('display1').style.display="none";
-    document.getElementById('display2').style.display="none";
-    document.getElementById('display3').style.display="none";
-    
-    document.getElementById('display' + tab).style.display="block";  
-}
